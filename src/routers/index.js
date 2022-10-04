f309ac3e5d56c453feb0aa8603c5d51e6cba0da3
Kevin <jesseha96@gmail.com>
2022-10-04 19:21:14 +0700

    create navigate router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,44 +1,49 @@
-import { Routes, Route, Link, Outlet } from "react-router-dom";
-import { LandingPage } from "../screens/LandingPage";
-import { Posts } from "../screens/Posts";
-import { PostDetails } from "../screens/PostDetails";
-import { ErrorPage } from "../screens/ErrorPage";
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import { LandingPage } from '../screens/LandingPage';
+import { Posts } from '../screens/Posts';
+import { PostDetails } from '../screens/PostDetails';
+import { ErrorPage } from '../screens/ErrorPage';
+import Testing from '../screens/Testing/Testing';
 
 const Layout = () => {
-  return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
+    return (
+        <div>
+            {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Landing Page</Link>
-          </li>
-          <li>
-            <Link to="/posts">Posts</Link>
-          </li>
-        </ul>
-      </nav>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Landing Page</Link>
+                    </li>
+                    <li>
+                        <Link to='/posts'>Posts</Link>
+                    </li>
+                    <li>
+                        <Link to='/testing'>Testing</Link>
+                    </li>
+                </ul>
+            </nav>
 
-      <hr />
-      <Outlet />
-    </div>
-  );
+            <hr />
+            <Outlet />
+        </div>
+    );
 };
 
 const CustomRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<LandingPage />} />
-      </Route>
-      <Route path="/posts" element={<Layout />}>
-        <Route index element={<Posts />} />
-      </Route>
-      <Route path="/posts/:postId" element={<PostDetails />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<LandingPage />} />
+                <Route path='testing' element={<Testing />} />
+            </Route>
+            <Route path='/posts' element={<Layout />}>
+                <Route index element={<Posts />} />
+            </Route>
+            <Route path='/posts/:postId' element={<PostDetails />} />
+            <Route path='*' element={<ErrorPage />} />
+        </Routes>
+    );
 };
 
 export default CustomRouter;
